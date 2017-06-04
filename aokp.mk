f368025423242ead011d5cfe14982a4f078e5a5a
teja98 <tejachaitu96@gmail.com>
2017-06-04 04:52:31 +0000

    AOKP-ify
    Signed-off-by: teja98 <tejachaitu96@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := aokp_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
